292f338e97bd19925e19ae7bec916b1ebc2e6b7c
Abhishek <dob1997vyas@gmail.com>
2025-09-09 01:48:49 +0530

    cart page new code has been added after naveen's comments

--- src/main/java/com/qa/opencart/pages/cartpage.java
+++ src/main/java/com/qa/opencart/pages/cartpage.java
@@ -1,5 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class cartpage {
+	
+	String cart = "cart";
+	By carts = By.id("cart");
+	
+	public void addtocart() {
+		System.out.println("adding to cart");
+		
+	}
 
 }
