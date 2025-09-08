7700f3f81141434383a2c4d1b026bd372ebf9a5b
Abhishek <dob1997vyas@gmail.com>
2025-09-09 01:23:49 +0530

    added cart test

---
+++ src/test/java/com/qa/opencart/tests/cartpagetest.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.tests;
+
+import org.testng.annotations.Test;
+
+public class cartpagetest {
+
+	@Test
+	public void carttest() {
+		System.out.println("cart testing");
+	}
+	
+}
